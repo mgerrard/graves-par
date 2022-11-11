9b75662475a6faf37f75471b8826fd6c9dc5d739
mjg6v <mjg6v@pop-os.localdomain>
2022-11-11 11:18:48 -0500

    update to 3 or 8 flags

--- graves.sh
+++ graves.sh
@@ -6,10 +6,14 @@ if [[ $1 == *"--version"* ]]; then
     exit 0
 fi
 
+THREE_ARGS=false;
 # check args
-if [ "$#" -ne 8 ]; then
-    echo "oops, expecting eight parameters to this script"
-    echo "  usage: ./graves.sh verifier-parallel-portfolio.cvt --cache-dir cache --no-cache-update --use-python-processes
+if [ "$#" -eq 3 ]; then
+    THREE_ARGS=true;
+elif [ "$#" -ne 8 ]; then
+    echo "oops, expecting either three or eight parameters to this script"
+    echo "  basic usage: ./graves.sh <C_FILE> <SPEC> {ILP32,LP64}"
+    echo "  sv-comp usage: ./graves.sh verifier-parallel-portfolio.cvt --cache-dir cache --no-cache-update --use-python-processes
 <C_FILE> <SPEC> {ILP32,LP64}"
     exit 9
 fi
@@ -27,4 +31,8 @@ tool3=${arr_result[2]}
 sed -e "s/REPLACE1/$tool1/g" -e "s/REPLACE2/$tool2/g" -e "s/REPLACE3/$tool3/g" graves-template.cvt > verifier-parallel-portfolio.cvt
 
 # launch coveriteam harness
-./bin/coveriteam $1 $2 $3 $4 $5 --input program_path=$6 --input specification_path=$7 --data-model=$8
+if [ "$THREE_ARGS"=true ]; then
+    ./bin/coveriteam --input program_path=$1 --input specification_path=$2 --data-model=$3 verifier-parallel-portfolio.cvt
+else
+    ./bin/coveriteam $1 $2 $3 $4 $5 --input program_path=$6 --input specification_path=$7 --data-model=$8
+fi
